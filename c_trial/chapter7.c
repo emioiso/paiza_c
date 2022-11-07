ef90f6fb5e62720a9768659c839d07f8852ec107
emioiso <jijjjiijiijijiji@gmail.com>
2022-11-07 19:03:08 +0900

    #7プログラムで計算する

---
+++ c_trial/chapter7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("%d\n", 100 + 30);
+  printf("%d\n", 100 - 30);
+  printf("%d\n", 100 * 30);
+  printf("%d\n", 100 / 30);
+  printf("%d\n", 100 + 30 + 2);
+  printf("%d\n", 100 + 30 * 2);
+  printf("%d\n", (100 + 30) * 2);
+}
