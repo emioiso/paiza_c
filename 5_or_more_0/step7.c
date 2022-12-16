1e8ab976fa1881f7af1da54601b98dd28530d4e2
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-17 00:03:38 +0900

    STEP: 7 インクリメント

---
+++ 5_or_more_0/step7.c
@@ -0,0 +1,13 @@
+//https://paiza.jp/works/mondai/prob60/total_value_0
+#include <stdio.h>
+int main(void)
+{
+    int n;
+
+    scanf("%d",&n);
+    // printf("%d",n);
+
+    printf("%d\n",n + 1);
+
+    return 0;
+}
