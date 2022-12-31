dedb4134306e8ac31691aa25dcb974f232746d07
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-31 18:33:23 +0900

    STEP:10 整数の足し算

---
+++ 5_or_more_0/step10.c
@@ -0,0 +1,13 @@
+//https://paiza.jp/works/mondai/prob60/total_value_3
+#include <stdio.h>
+int main(void){
+
+    int a;
+    int b;
+
+    scanf("%d %d\n",&a,&b);
+    // printf("%d %d\n",a,b);
+
+    printf("%d\n",a + b);
+    return 0;
+}
