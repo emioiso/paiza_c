d759abdb293ea3b589ec37e2b35ee675d6474681
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-31 19:18:26 +0900

    STEP: 7 i番目の出力 1

---
+++ array_primer/step4.c
@@ -0,0 +1,15 @@
+//https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_i-thoutput_step1
+#include <stdio.h>
+int main(void){
+
+    int k;
+
+    scanf("%d",&k);
+    // printf("%d\n",k);
+
+    int a[11] = {1,3,5,4,6,2,1,7,1,5};
+
+    printf("%d",a[k -1]);
+
+    return 0;
+}
