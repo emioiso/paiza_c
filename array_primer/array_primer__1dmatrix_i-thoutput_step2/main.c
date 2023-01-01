fcb94d2ec34c0c1fb2e76ca89c31d48b044c16af
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-01 15:44:17 +0900

    STEP: 8 i番目の出力 2

---
+++ array_primer/array_primer__1dmatrix_i-thoutput_step2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void){
+
+    int k;
+
+    scanf("%d",&k);
+    // printf("%d\n",k);
+
+    int a;
+    for(int i = 0; i < k; i++){
+
+        scanf("%d",&a);
+        // printf("%d",a);
+    }
+    printf("%d\n",a);
+
+    return 0;
+}
