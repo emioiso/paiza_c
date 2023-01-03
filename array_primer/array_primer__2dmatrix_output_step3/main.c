13405db4644e4a1d68828cb4a2fa644fee495205
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-03 14:10:11 +0900

    STEP: 12 行数の出力

--- array_primer/array_primer__2dmatrix_output_step3/main.c
+++ array_primer/array_primer__2dmatrix_output_step3/main.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 
+int	n[3][4] = {
+		{1, 2, 3, 4},
+		{6, 5, 4, 3},
+		{3, 1, 8, 1},
+	};
+
 int	main(void)
 {
 
-	int	n[3][3] = {
-		{1, 2, 3},
-		{4, 5, 6},
-		{8, 1, 3},
-	};
+	// printf("%d\n",n[2][3]);
 
-	printf("%d\n",n[0][2]);
+	// printf("%p\n",&n[0]);
+	// printf("%p\n",&n[1]);
+	// printf("%p\n",&n[2]);
+	// printf("%p\n",&n[3]);
+	// printf("%p\n",&n[4]);
 
-	return (0);
+	// printf("%d\n",n[0][0]);
+	// printf("%d\n",n[1][0]);
+	// printf("%d\n",n[2][0]);
+	// printf("%d\n",n[3][0]);
+
+	// printf("%d\n", sizeof(n));
+	// printf("%d\n", sizeof(n[0]));
+
+	// C言語の配列の個数を出すやり方
+	// printf("%d\n", sizeof(n) / sizeof(n[0]));
+	// jsの場合
+	// length(n);
+
+	int count = 0;
+	for (int i = 0; i < sizeof(n) / sizeof(n[0]); i++)
+	{
+		// printf("%d\n", n[i][0]);
+		count++;
+	}
+	printf("%d\n",count);
 }
