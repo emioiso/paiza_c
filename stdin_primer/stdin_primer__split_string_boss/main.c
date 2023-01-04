859c717d99884f262837f1c6d7572cf037dfa943
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-04 23:59:11 +0900

    FINAL問題 【半角スペース区切りでの文字列の分割】5 つの文字列の半角スペース区切りでの分割

---
+++ stdin_primer/stdin_primer__split_string_boss/main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int	main(void)
+{
+
+	char n1[4] = "one";
+	char n2[4] = "two";
+	char n3[6] = "three";
+	char n4[5] = "four";
+	char n5[5] = "five";
+
+	printf("%s\n%s\n%s\n%s\n%s\n",n1,n2,n3,n4,n5);
+
+	return (0);
+}
