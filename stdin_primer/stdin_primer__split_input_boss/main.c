a01c59956b313ea0a00127af349025578d469b54
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-05 14:32:47 +0900

    FINAL問題 【整数の行入力】1,000行の整数の入力

---
+++ stdin_primer/stdin_primer__split_input_boss/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	char s[11];
+
+	// scanf("%s",s);
+	// printf("%s\n",s);
+
+	for (int i = 0; i < 1000; i++)
+	{
+		scanf("%s",s);
+		printf("%s\n",s);
+	}
+
+	return (0);
+}
