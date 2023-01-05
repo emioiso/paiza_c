04f994a615b2e9aee99d81b8f95eb25ba878ed60
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-05 17:10:43 +0900

    STEP: 24 改行区切りでの N 個の整数の入力

---
+++ stdin_primer/stdin_primer__integer_number_step1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	n;
+
+	scanf("%d",&n);
+	// printf("%d\n",n);
+
+	int	a;
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d",&a);
+		printf("%d\n",a);
+	}
+	return (0);
+}
