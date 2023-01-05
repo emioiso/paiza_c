fad7129b797d7b582c4df2ff1f7840c79e551fe0
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-05 17:27:30 +0900

    STEP: 26 1 行目で与えられる N 個の整数の入力

---
+++ stdin_primer/stdin_primer__integer_number_step3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	n;
+
+	scanf("%d",&n);
+	// printf("%d\n",n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		scanf("%d",&a);
+		printf("%d\n", a);
+	}
+	return (0);
+}
