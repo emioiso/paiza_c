9e54c868cd73fa1afd979578e6c12a6fc24e36e2
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-05 17:01:07 +0900

    FINAL問題 【整数の半角スペース区切りの入力】1,000個の整数の半角スペース区切りの入力

---
+++ stdin_primer/stdin_primer__integer_space_boss/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	a;
+
+	// scanf("%d",&a);
+	// printf("%d\n",a);
+
+	for (int i = 0; i < 1000; i++)
+	{
+		scanf("%d",&a);
+		printf("%d\n",a);
+	}
+
+
+	return (0);
+}
