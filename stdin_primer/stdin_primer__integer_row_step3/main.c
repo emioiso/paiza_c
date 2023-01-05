579c0e4a46f09f81ed64d9cd0c8a36452cacba3b
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-05 19:32:32 +0900

    STEP: 16 5 行の整数の入力

---
+++ stdin_primer/stdin_primer__integer_row_step3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	// int	a;
+
+	// scanf("%d",&a);
+	// printf("%d\n",a);
+
+	for (int i = 0; i < 5; i++)
+	{
+		int	a;
+
+		scanf("%d",&a);
+		printf("%d\n",a);
+	}
+	return (0);
+}
