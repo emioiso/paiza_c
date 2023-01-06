04abf8468ed98c5fdb0caee2f42dbecdfcf4a330
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-06 18:24:28 +0900

    STEP: 4 数字の受け取り 2

---
+++ loop_problems/loop_problems__input_num_step2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	n;
+
+	scanf("%d",&n);
+	// printf("%d\n",n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int k;
+		scanf("%d",&k);
+		printf("%d\n",k);
+	}
+	return (0);
+}
