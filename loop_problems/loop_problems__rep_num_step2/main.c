64732b7087c28bf338cf217c9faf8db5db77206e
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-07 04:50:31 +0900

    STEP: 6 ある数をある回数表示 2

---
+++ loop_problems/loop_problems__rep_num_step2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	n;
+	int	k;
+
+	scanf("%d%d",&n,&k);
+	// printf("%d %d\n",n,k);
+
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
