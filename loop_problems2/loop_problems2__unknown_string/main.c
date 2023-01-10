7e7486af8c764a0c1d67a2aaa63aca292f6ad216
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-11 01:15:37 +0900

    STEP: 2 未知数個の文字列の受け取り

---
+++ loop_problems2/loop_problems2__unknown_string/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char	n[11];
+	// scanf("%s",n);
+	// printf("%s\n", n);
+
+	for (int i = 0; n[0] != 'E'; i++)
+	{
+		scanf("%s",n);
+		printf("%s\n", n);
+	}
+	return (0);
+}
