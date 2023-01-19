fc617c4b83b1eec630d01952d27735277be3db84
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-19 17:17:40 +0900

    STEP: 14 文字列の長さ

---
+++ 5_or_more_0/substring_2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int	main(void)
+{
+
+	char	s[31];
+
+	scanf("%s",s);
+	// printf("%s\n",s);
+
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		// printf("%d\n", i);
+	}
+	printf("%d\n", i);
+
+	return (0);
+}
