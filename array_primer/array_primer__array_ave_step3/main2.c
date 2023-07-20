20ca450aabcc058cc5ef77ca908330a3b37d736e
emioiso <jijjjiijiijijiji@gmail.com>
2023-07-20 20:15:01 +0900

    STEP: 61 ある数以上以下の要素の列挙 3_平均値の少数を条件分岐で分岐するver.

---
+++ array_primer/array_primer__array_ave_step3/main2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	n;
+
+	scanf("%d",&n);
+// 	printf("%d\n",n);
+
+	int a[n];
+	int totalA = 0;
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d",&a[i]);
+// 		printf("%d ",a[i]);
+		totalA += a[i];
+	}
+// 	printf("\n");
+ // printf("totalA = %d\n",totalA);
+    int aveA = totalA / n;
+    if(totalA % n != 0){
+        aveA++;
+    }
+    // printf("aveA = %f\n",aveA);
+
+    for (int i = 0; i < n; i++)
+	{
+	    if(aveA <= a[i]){
+	        printf("%d\n",a[i]);
+	    }
+	}
+	return (0);
+}
