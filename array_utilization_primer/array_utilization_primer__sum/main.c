a478b42b4e8b122b8e296f94f2ac69b183c20faa
emioiso <jijjjiijiijijiji@gmail.com>
2023-07-24 23:41:48 +0900

    STEP: 2 全ての要素の和

---
+++ array_utilization_primer/array_utilization_primer__sum/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int	main(void)
+{
+	int	n;
+
+	scanf("%d",&n);
+	printf("%d\n",n);
+
+	int a[n];
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d",&a[i]);
+		printf("%d",a[i]);
+	}
+
+	return (0);
+}
