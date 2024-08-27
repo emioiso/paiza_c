622bab89d817d67d9ade295d6303edd560614706
emioiso <jijjjiijiijijiji@gmail.com>
2024-08-27 15:16:23 +0900

    STEP: 12 2 つの文字列を出力

---
+++ stdout_primer/stdout_primer__string_output_step2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // char name[3][5] = { "Rena" , "Yuki" , "Mimi" };
+    char str[3][9] = {"paiza","learning"};
+
+    // printf("%s\n%s\n%s" , name[0] , name[1] , name[2]);
+    printf("%s %s\n" , str[0] , str[1]);
+
+    return 0;
+}
