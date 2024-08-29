90a2cbfd26e98c81d514b3275099f872092c35c8
emioiso <jijjjiijiijijiji@gmail.com>
2024-08-30 02:43:49 +0900

    STEP: 6 2 つの数値を出力

---
+++ stdout_primer/stdout_primer__space_oneline_step1_retry1.c
@@ -0,0 +1,11 @@
+int main(void){
+    int num = 1;
+    for(int i = 0; i < 2; i++){
+        if(i != 1){
+            printf("%d ",num);
+        } else {
+            printf("%d\n",num);
+        }       
+    }
+    return(0);
+}
