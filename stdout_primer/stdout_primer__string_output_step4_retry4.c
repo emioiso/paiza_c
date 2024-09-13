56372674aa9915b18d521195defc7cbb3a05cd8a
emioiso <jijjjiijiijijiji@gmail.com>
2024-09-13 17:45:15 +0900

    STEP: 14 入力された 10 個の文字列を出力

--- stdout_primer/stdout_primer__string_output_step4_retry4.c
+++ stdout_primer/stdout_primer__string_output_step4_retry4.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
-int main(void){
-    // 標準入力から入力される文字列の制約を確認する
-    // char s[15];
-    // char t[15];
-    
-    // scanf();を使って標準入力から値を受け取ってみる→成功
-    // scanf("%s",s);
-    // printf("%s\n",s);
-    // scanf("%s",t);
-    // printf("%s\n",t);
-    
-    
-    
-    // char str[30];
-    
-    // // scanf();とprintf();を二回記述するのめんどくさいからfor文を使って標準入力から受け取る→
-    // for(int i = 0; i < 2; i++){
-    //     scanf("%s",str);
-    //     printf("%s\n",str);
-    // }
-    
-    // fgetsで標準入力から文字列を受け取る
-    char str[30];
-    
-    for(int i = 0; i < 2; i++){
-        fgets(str,sizeof(str),stdin);
-        printf("%s",str);
+#include <string.h>
+
+int main(void)
+{
+    char s[12];
+
+    for(int i = 0; i < 10; i++){
+        fgets(s,sizeof(s),stdin);
+        // printf("%s",s);    
+
+        // 改行文字があれば取り除く
+        s[strcspn(s, "\n")] = '\0';  
+
+        // 最初の文字列はスペースをつけずに出力、それ以降はスペース付きで出力
+        if (i == 0) {
+            printf("%s", s);
+        } else {
+            printf(" %s", s);  // 2つ目以降はスペースを前に付けて出力
+        }
     }
-    return(0);
+    printf("\n");  // 最後に改行を追加
+    
+    return 0;
 }
