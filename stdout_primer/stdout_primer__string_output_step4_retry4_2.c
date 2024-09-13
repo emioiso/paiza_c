38ca89cad33e342988489e7921cd3a896a90bcbf
emioiso <jijjjiijiijijiji@gmail.com>
2024-09-13 17:43:30 +0900

    STEP: 14 入力された 10 個の文字列を出力

---
+++ stdout_primer/stdout_primer__string_output_step4_retry4_2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+main(void){
+    str[11][11];
+    for(int i = 0; i < 0; i++){
+        scanf("%s",str[i]);
+        printf("%s",str[i]);
+    }
+    return(0);
+}
+
+// #include <stdio.h>
+
+// int main() {
+//     char s[10][11];  // 各文字列の長さは最大10なので、11文字（終端文字含む）を確保
+//     for (int i = 0; i < 10; i++) {
+//         scanf("%s", s[i]);  // 10回文字列を入力
+//     }
+    
+//     for (int i = 0; i < 10; i++) {
+//         if (i > 0) {
+//             printf(" ");  // 2番目以降の文字列の前にスペースを挿入
+//         }
+//         printf("%s", s[i]);  // 各文字列を出力
+//     }
+    
+//     printf("\n");  // 最後に改行を追加
+//     return 0;
+// }
