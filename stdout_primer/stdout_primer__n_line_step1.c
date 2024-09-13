6d676d879377c525464a92955c2f7f6b847cd961
emioiso <jijjjiijiijijiji@gmail.com>
2024-09-13 18:44:12 +0900

    STEP: 16 1 行または 2 行の出力

---
+++ stdout_primer/stdout_primer__n_line_step1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+    int n;
+    scanf("%d",&n);
+    
+    if(n == 2){
+        printf("1\n2\n");
+    } else {
+        printf("1\n");
+    }
+    return 0;
+}
