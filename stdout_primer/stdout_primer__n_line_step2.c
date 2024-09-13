a8f7cfc37e43c90681f5ab20ee629d02b8cf4800
emioiso <jijjjiijiijijiji@gmail.com>
2024-09-13 18:56:04 +0900

    STEP: 17 数行の出力

---
+++ stdout_primer/stdout_primer__n_line_step2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    int n;
+    scanf("%d",&n);  
+    // printf("%d",n);   
+    
+    for(int i = 1; i <= n; i++){
+        printf("%d\n",i);    
+    }
+    return 0;
+}
