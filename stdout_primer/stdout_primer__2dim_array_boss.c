fd8ed5a16a3b4d37fb3326e6c6137d3d574e26ee
emioiso <jijjjiijiijijiji@gmail.com>
2024-09-27 19:38:21 +0900

    FINAL問題 【n * n の 2 次元配列の表示】N * N の九九表の出力

---
+++ stdout_primer/stdout_primer__2dim_array_boss.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void){
+    int n;
+    scanf("%d",&n);
+    // printf("%d",n);
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n){
+                printf("%d\n",i*j);    
+            } else {
+                printf("%d ",i*j);
+            }
+        }  
+    }
+    // printf("\n");
+    return 0;
+}
