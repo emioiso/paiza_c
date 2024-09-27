009609630a4ae1358b0bc7f90349f07d164797c6
emioiso <jijjjiijiijijiji@gmail.com>
2024-09-27 17:42:20 +0900

    STEP: 29 九九表の出力

---
+++ stdout_primer/stdout_primer__2dim_array_step3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+    for(int i = 1; i <= 9; i++){
+        for(int j = 1; j <= 9; j++){
+            if(j % 9 == 0){
+                printf("%d\n",i*j);    
+            } else {
+                printf("%d ",i*j);
+            }
+        }   
+    }
+    return 0;
+}
