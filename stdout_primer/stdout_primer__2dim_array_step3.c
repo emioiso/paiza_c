841f055e389bf165e9206c3de6b7a3a3aa4b7b01
emioiso <jijjjiijiijijiji@gmail.com>
2024-09-27 19:44:08 +0900

    STEP: 29 九九表の出力

--- stdout_primer/stdout_primer__2dim_array_step3.c
+++ stdout_primer/stdout_primer__2dim_array_step3.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 int main(void){
-    int n;
-    scanf("%d",&n);
-    // printf("%d",n);
-    
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            if(j == n){
-                printf("%d\n",i*j);    
+    for(int i = 1; i <= 9; i++){
+        for(int j = 1; j <= 9; j++){
+            if(j == 9){
+                printf("%d\n",i * j);    
             } else {
-                printf("%d ",i*j);
+                printf("%d ",i * j);
             }
-        }  
+        }   
     }
     return 0;
 }
